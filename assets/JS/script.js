4fe20728505c28e5c91675ad4a0d93cd003adbf9
Heath Bennett <heathben418@gmail.com>
2024-09-29 16:56:15 -0400

    finished html started javaScript

---
+++ assets/JS/script.js
@@ -0,0 +1,7 @@
+
+const address = document.getElementById('address');
+const deliveryFee = document.getElementById('deliveryFee');
+const orderType = document.getElementById('orderType');
+const cashTip = document.getElementById('cashTip');
+const gratuity = document.getElementById('gratuity');
+const orderTotal = document.getElementById('orderTotal');
