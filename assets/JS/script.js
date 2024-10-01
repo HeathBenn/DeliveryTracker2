80e28a48ea00f14e3e137aa76cd67a3a2433fc98
Heath Bennett <heathben418@gmail.com>
2024-10-01 12:31:10 -0400

    more work on object. able to add data to table

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,19 +6,32 @@ const cashTip = document.getElementById('cashTip');
 const gratuity = document.getElementById('gratuity');
 const orderTotal = document.getElementById('orderTotal');
 const submitButton = document.getElementById('submit');
-
-let stopNumber = 1;
+const table = document.getElementById('deliveries');
 const deliveries = [];
+let stopNumber = 1;
 
 submitButton.onclick = function() {submitDelivery()};
 
-function submitDelivery(){
-    const newDelivery = new ConstructDelivery(address.value, orderType.value, orderTotal.value, gratuity.value, deliveryFee.value);
-    newDelivery.delivery();
-    console.log(deliveries);
+//Gets Hour Minute and AM/PM
+function timeStamp() {
+    const time = new Date();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+    const formatHours = hours % 12 || 12;
+    return `${formatHours}:${minutes.toString().padStart(2,'0')} ${meridiem}`;
 }
 
+
+//Ensures correct format for dollar amounts
+const formatter = new Intl.NumberFormat('en-US',{
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
 function ConstructDelivery(address, type, total, tip, fee) {
+    this.stopNumber = stopNumber;
+    this.timeLog = timeStamp();
     this.address = address;
     this.orderType = type;
     this.orderTotal = total;
@@ -27,4 +40,26 @@ function ConstructDelivery(address, type, total, tip, fee) {
     this.delivery = function(){
         deliveries.push(this);
     };
+    this.populate = function(){
+        let template = `
+        <tr>
+        <td>${stopNumber}</td>
+        <td>${this.address}</td>
+        <td>${this.orderType}</td>
+        <td>${this.orderTotal}</td>
+        <td>${this.gratuity}</td>
+        <td>${this.deliveryFee}</td>
+        </tr>
+        `
+        table.innerHTML += template;
+        stopNumber += 1;
+    };
+}
+
+//Submits the delivery info
+function submitDelivery(){
+    const newDelivery = new ConstructDelivery(address.value, orderType.value, orderTotal.value, gratuity.value, deliveryFee.value);
+    newDelivery.delivery();
+    newDelivery.populate();
+    console.log(deliveries);
 }
